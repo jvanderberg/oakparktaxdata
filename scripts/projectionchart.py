0a5514f1971472adccc6f5b04b4d0ef0fe20e8a7
Josh Vanderberg <jvanderberg@gmail.com>
2018-06-16 11:38:25 -0500

    Updated projection

--- scripts/projectionchart.py
+++ scripts/projectionchart.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 final = pd.read_csv("projections.csv")
+final = final.set_index(final.Year)
 plt.close()
-plt.figure(figsize=(7,6), dpi=200)
+plt.figure(figsize=(5,4), dpi=200)
 plt.ylabel("Tax Levy $ (Millions)")
 plt.title('Projected Future Tax Levy')
 line1=plt.plot(final.Levy / 1000000, color='blue')
 line2=plt.plot(final.Fit / 1000000, color='red')
 plt.legend(['Actual', 'Projection'])
 plt.grid(axis='y', linewidth=0.5)
-
+plt.xticks([1999, 2018, 2038])
 plt.savefig('charts/projected future levy.png')
