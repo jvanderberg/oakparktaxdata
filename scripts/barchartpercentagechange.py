3d30932f9c2077e261afa4520e5d2136917b97a4
Josh Vanderberg <jvanderberg@gmail.com>
2019-08-24 22:36:09 +0000

    Add total levy pct chart

--- scripts/barchartpercentagechange.py
+++ scripts/barchartpercentagechange.py
@@ -40,3 +40,26 @@ plt.legend((p1[0], p2[0], p3[0], p4[0], p5[0]), ('D200', 'D97',
                                                  'Oak Park Township', 'Park District', 'Village of Oak Park'), loc='lower left')
 
 plt.savefig(str(taxyear)+'/charts/percentage change levy by year.png')
+plt.close()
+
+plt.figure(figsize=(7, 6), dpi=200)
+all = change['All'] * 100
+N=4
+rolling = pd.Series(all.values).rolling(window=N).mean().iloc[N-1:].values
+print(taxyear-baseyear-rolling.size)
+rolling = np.pad(rolling, (taxyear-baseyear-rolling.size+1,0), 'constant', constant_values=np.NaN)
+print(all)
+df = pd.DataFrame({'total': all, 'rolling': rolling})
+print(df)
+year = np.array(pivot.Year).astype(int)
+p1 = plt.plot(df['total'], color='#3366cc')
+p2 = plt.plot(df['rolling'], color='#dc3912')
+plt.xticks(np.arange(baseyear, taxyear, step=5))
+plt.ylabel("Percentage Increase")
+plt.title('Yearly % Increase Oak Park Total Levy')
+
+plt.xticks(np.arange(baseyear, taxyear, step=5))
+plt.grid(axis='y', linewidth=0.5)
+plt.legend((p1[0], p2[0]),('Year over Year', str(N)+' Yr. Rolling avg'), loc='lower left')
+
+plt.savefig(str(taxyear)+'/charts/percentage change total levy by year.png')
