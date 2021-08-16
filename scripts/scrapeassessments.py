792989b584d95aef36ccdf0342944c7954ea392c
Josh Vanderberg <josh@yesenergy.com>
2021-08-16 12:22:56 +0000

    2020 tax year update

--- scripts/scrapeassessments.py
+++ scripts/scrapeassessments.py
@@ -15,7 +15,7 @@ taxyear = common.getTaxYear()
 pins = pd.read_csv("oppins.csv")
 
 # Don't start processing pins until you hit 'startpin'
-# startpin = '16-05-100-035-0000'
+startpin = '16-08-121-047-0000'
 try:
     startpin
 except NameError:
@@ -73,51 +73,68 @@ for index, pin in pins.iterrows():
         continue
     print(pin.PIN)
 
-    url = 'https://www.cookcountyassessor.com/Property.aspx?mode=details&pin=' + \
+    url = 'https://www.cookcountyassessor.com/pin/' + \
         pin.PIN.replace("-", "")
     time.sleep(0.25)
     contents = simple_get(url)
+    print(url)
+
     html = BeautifulSoup(contents, 'html.parser')
 
-    items = [{'pin': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoPIN'},
-             {'address': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoAddress'},
-             {'city': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoCity'},
-             {'township': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoTownship'},
-             {'classification': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoClassification'},
-             {'landsqft': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoSqFt'},
-             {'neighborhood': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoNBHD'},
-             {'taxcode': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoTaxcode'},
-             {'year': 'ctl00_phArticle_ctlPropertyDetails_lblPropInfoCurYear'},
-             {'landassessedvalue': 'ctl00_phArticle_ctlPropertyDetails_lblAsdValLandFirstPass'},
-             {'buildingassessedvalue': 'ctl00_phArticle_ctlPropertyDetails_lblAsdValBldgFirstPass'},
-             {'combinedassessedvalue': 'ctl00_phArticle_ctlPropertyDetails_lblAsdValTotalFirstPass'},
-             {'landassessedvaluepreviousyear': 'ctl00_phArticle_ctlPropertyDetails_lblAsdValLandCertified'},
-             {'buildingassessedvaluepreviousyear': 'ctl00_phArticle_ctlPropertyDetails_lblAsdValBldgCertified'},
-             {'combinedassessedvaluepreviousyear': 'ctl00_phArticle_ctlPropertyDetails_lblAsdValTotalCertified'},
-             {'marketvalue': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharMktValCurrYear'},
-             {'marketvaluepreviousyear': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharMktValPrevYear'},
-             {'description': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharDesc'},
-             {'residencetype': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharResType'},
-             {'use': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharUse'},
-             {'apartments': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharApts'},
-             {'exteriorconstruction': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharExtConst'},
-             {'fullbaths': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharFullBaths'},
-             {'halfbaths': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharHalfBaths'},
-             {'basement': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharBasement'},
-             {'attic': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharAttic'},
-             {'centralair': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharCentAir'},
-             {'fireplaces': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharFrpl'},
-             {'garage': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharFrpl'},
-             {'age': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharAge'},
-             {'buildingsqft': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharBldgSqFt'},
-             {'assessmentpass': 'ctl00_phArticle_ctlPropertyDetails_lblPropCharAsmtPass'}
+    items = [{'pin': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div.col-md-4.col-md-offset-4 > div > div:nth-child(1) > span.detail-row--detail'},
+             {'address': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div.col-md-4.col-md-offset-4 > div > div:nth-child(2) > span.detail-row--detail'},
+             {'city': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div.col-md-4.col-md-offset-4 > div > div:nth-child(3) > span.detail-row--detail'},
+             {'township': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div.col-md-4.col-md-offset-4 > div > div:nth-child(4) > span.detail-row--detail'},
+             {'classification': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(1) > span.detail-row--detail.large'},
+             {'landsqft': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(2) > span.detail-row--detail.large'},
+             {'neighborhood': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(3) > span.detail-row--detail.large'},
+             {'taxcode': 'body > div.dialog-off-canvas-main-canvas > div.main-container.container.js-quickedit-main-content > div > section > div.region.region-content > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(4) > span.detail-row--detail.large'},     
+             {'landassessedvalue': '#collapseOne > div > div > div:nth-child(4) > div.col-xs-4'},
+             {'buildingassessedvalue': '#collapseOne > div > div > div:nth-child(5) > div.col-xs-4'},
+             {'combinedassessedvalue': '#collapseOne > div > div > div:nth-child(3) > div.col-xs-4'},
+             {'landassessedvaluepreviousyear': '#collapseOne > div > div > div:nth-child(4) > div.col-xs-5'},
+             {'buildingassessedvaluepreviousyear': '#collapseOne > div > div > div:nth-child(5) > div.col-xs-5'},
+             {'combinedassessedvaluepreviousyear': '#collapseOne > div > div > div:nth-child(3) > div.col-xs-5'},
+             {'marketvalue': '#collapseOne > div > div > div:nth-child(2) > div.col-xs-4'},
+             {'marketvaluepreviousyear': '#collapseOne > div > div > div:nth-child(2) > div.col-xs-5'},
+             {'description': '#collapseTwo > div > div:nth-child(1) > span.detail-row--detail'},
+             {'residencetype': '#collapseTwo > div > div:nth-child(2) > span.detail-row--detail'},
+             {'use': '#collapseTwo > div > div:nth-child(3) > span.detail-row--detail'},
+             {'exteriorconstruction': '#collapseTwo > div > div:nth-child(4) > span.detail-row--detail'},
+             {'fullbaths': '#collapseTwo > div > div:nth-child(5) > span.detail-row--detail'},
+             {'halfbaths': '#collapseTwo > div > div:nth-child(6) > span.detail-row--detail'},
+             {'basement': '#collapseTwo > div > div:nth-child(7) > span.detail-row--detail'},
+             {'attic': '#collapseTwo > div > div:nth-child(8) > span.detail-row--detail'},
+             {'centralair': '#collapseTwo > div > div:nth-child(9) > span.detail-row--detail'},
+             {'fireplaces': '#collapseTwo > div > div:nth-child(10) > span.detail-row--detail'},
+             {'garage': '#collapseTwo > div > div:nth-child(11) > span.detail-row--detail'},
+             {'age': '#collapseTwo > div > div:nth-child(12) > span.detail-row--detail'},
+             {'buildingsqft': '#collapseTwo > div > div:nth-child(13) > span.detail-row--detail'},
+             {'assessmentpass': '#collapseTwo > div > div:nth-child(14) > span.detail-row--detail'},
+             {'exemption1year': '#collapseFour > div > div > div > div:nth-child(2) > div.col-xs-3.pt-header'},
+             {'exemption1owner': '#collapseFour > div > div > div > div:nth-child(2) > div:nth-child(2)'},
+             {'exemption1senior': '#collapseFour > div > div > div > div:nth-child(2) > div:nth-child(3)'},
+             {'exemption1seniorfreeze': '#collapseFour > div > div > div > div:nth-child(2) > div:nth-child(4)'},
+             {'exemption2year': '#collapseFour > div > div > div > div:nth-child(3) > div.col-xs-3.pt-header'},
+             {'exemption2owner': '#collapseFour > div > div > div > div:nth-child(3) > div:nth-child(2)'},
+             {'exemption2senior': '#collapseFour > div > div > div > div:nth-child(3) > div:nth-child(3)'},
+             {'exemption2seniorfreeze': '#collapseFour > div > div > div > div:nth-child(3) > div:nth-child(4)'},
+             {'exemption3year': '#collapseFour > div > div > div > div:nth-child(4) > div.col-xs-3.pt-header'},
+             {'exemption3owner': '#collapseFour > div > div > div > div:nth-child(4) > div:nth-child(2)'},
+             {'exemption3senior': '#collapseFour > div > div > div > div:nth-child(4) > div:nth-child(3)'},
+             {'exemption3seniorfreeze': '#collapseFour > div > div > div > div:nth-child(4) > div:nth-child(4)'},
+             {'exemption4year': '#collapseFour > div > div > div > div:nth-child(5) > div.col-xs-3.pt-header'},
+             {'exemption4owner': '#collapseFour > div > div > div > div:nth-child(5) > div:nth-child(2)'},
+             {'exemption4senior': '#collapseFour > div > div > div > div:nth-child(5) > div:nth-child(3)'},
+             {'exemption4seniorfreeze': '#collapseFour > div > div > div > div:nth-child(5) > div:nth-child(4)'},
+
              ]
 
     row = pd.DataFrame()
     for item in items:
-        span = html.find_all(attrs={'id': list(item.values())[0]})
+        span = html.select(list(item.values())[0])
         try:
-            row[list(item.keys())[0]] = [span[0].get_text()]
+            row[list(item.keys())[0]] = [span[0].get_text().strip()]
 
         except:
             print('Error processing '+pin.PIN +
--- scripts/scrapecookcounty.py
+++ scripts/scrapecookcounty.py
@@ -4,19 +4,21 @@ import time
 import sys
 import splinter as splinter
 import common
+import selenium
 
 taxyear = common.getTaxYear()
 
 pins = pd.read_csv("oppins.csv")
+results = pd.DataFrame()
 
 # Don't start processing pins until you hit 'startpin'
-# startpin = '16-17-331-037-1012'
+startpin = '16-17-124-011-0000'
 try:
     startpin
+    results=pd.read_csv(str(taxyear)+'/cookcountyassessments.csv')
 except NameError:
     startpin = None
 
-results = pd.DataFrame()
 url = "http://www.cookcountypropertyinfo.com/default.aspx"
 
 
@@ -25,7 +27,6 @@ processpin = False
 if startpin == None:
     processpin = True
 
-results = pd.DataFrame()
 browser = Browser('firefox', headless=True)
 
 for index, pin in pins.iterrows():
@@ -33,81 +34,87 @@ for index, pin in pins.iterrows():
     valid = False
     retries = 0
     while (valid == False and retries < 10):
-        valid = True
-        if startpin != None and pin.PIN == startpin:
-            processpin = True
-            # As it can take awhile to get the the target pin, refresh the connection to the browser
-            # or it times out
-        if (processpin == False and index % 500 == 0):
-            print('Skipping: '+str(index))
-            browser.visit(url)
-        if (processpin == True):
-            if (index % 50 == 0):
-                #Prevent a memory leak - a real hack, but oh well
-                browser.quit()
-                browser = Browser('firefox', headless=True)
-            print(pin.PIN, index)
-            # print 2, pin
-            browser.visit(url)
-            parts = pin.PIN.split('-')
-            browser.fill(
-                "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox1", parts[0])
-            browser.fill(
-                "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox2", parts[1])
-            browser.fill(
-                "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox3", parts[2])
-            browser.fill(
-                "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox4", parts[3])
-            browser.fill(
-                "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox5", parts[4])
+        try:
+            valid = True
+            if startpin != None and pin.PIN == startpin:
+                processpin = True
 
-            button = browser.find_by_name(
-                'ctl00$ContentPlaceHolder1$PINAddressSearch$btnSearch')
+            if (processpin == True):
+                if (index % 50 == 0):
+                    #Prevent a memory leak - a real hack, but oh well
+                    browser.quit()
+                    browser = Browser('firefox', headless=True)
+                print(pin.PIN, index)
+                # print 2, pin
+                browser.visit(url)
+                parts = pin.PIN.split('-')
+                browser.fill(
+                    "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox1", parts[0])
+                browser.fill(
+                    "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox2", parts[1])
+                browser.fill(
+                    "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox3", parts[2])
+                browser.fill(
+                    "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox4", parts[3])
+                browser.fill(
+                    "ctl00$ContentPlaceHolder1$PINAddressSearch$pinBox5", parts[4])
 
-            button.click()
+                button = browser.find_by_name(
+                    'ctl00$ContentPlaceHolder1$PINAddressSearch$btnSearch')
 
-            baseyear = int(taxyear)
+                button.click()
 
-            # Interact with elements
-            if browser.is_element_present_by_id("ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_1", 10):
-                items = [{'taxbill'+str(baseyear): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_0'},
-                            {'taxbill'+str(
-                                baseyear-1): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_1'},
-                            {'taxbill'+str(
-                                baseyear-2): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_2'},
-                            {'taxbill'+str(
-                                baseyear-3): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_3'},
-                            {'taxbill'+str(
-                                baseyear-4): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_4'},
-                            {'homeownerexemption': 'ContentPlaceHolder1_TaxCalculator_lblHomeownerExemption'},
-                            {'seniorfreezeexemption': 'ContentPlaceHolder1_TaxCalculator_lblSeniorFreezeExemption'},
-                            {'seniorcitizenexemption': 'ContentPlaceHolder1_TaxCalculator_lblSeniorCitizenExemption'},
-                            {'taxbeforeexemptions': 'ContentPlaceHolder1_TaxCalculator_lblTaxBeforeExemptions'},
-                            {'taxafterexemptions': 'ContentPlaceHolder1_TaxCalculator_lblTaxAfterExemptions'},
-                            {'estimatedvalue': 'ContentPlaceHolder1_TaxYearInfo_propertyEstimatedValue'},
-                            {'assessedvalue': 'ContentPlaceHolder1_TaxYearInfo_propertyAssessedValue'},
-                            {'lotsize': 'ContentPlaceHolder1_TaxYearInfo_propertyLotSize'},
-                            {'buildingsize': 'ContentPlaceHolder1_TaxYearInfo_propertyBuildingSize'},
-                            {'class': 'ContentPlaceHolder1_TaxYearInfo_propertyClass'},
-                            {'taxcode': 'ContentPlaceHolder1_TaxYearInfo_propertyTaxCode'},
-                            {'address': 'ContentPlaceHolder1_PropertyInfo_propertyAddress'},
-                            {'city': 'ContentPlaceHolder1_PropertyInfo_propertyCity'},
-                            {'zip': 'ContentPlaceHolder1_PropertyInfo_propertyZip'},
-                            {'township': 'ContentPlaceHolder1_PropertyInfo_propertyTownship'},
-                            {'pin': 'ContentPlaceHolder1_lblResultTitle'}
-                            ]
+                baseyear = int(taxyear)
 
-                row = pd.DataFrame()
-                for item in items:
-                    try:
-                        value = browser.find_by_id(
-                            list(item.values())[0]).first.html
-                        row[list(item.keys())[0]] = [value]
+                # Interact with elements
+                if browser.is_element_present_by_id("ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_1", 10):
+                    items = [{'taxbill'+str(baseyear): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_0'},
+                                {'taxbill'+str(
+                                    baseyear-1): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_1'},
+                                {'taxbill'+str(
+                                    baseyear-2): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_2'},
+                                {'taxbill'+str(
+                                    baseyear-3): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_3'},
+                                {'taxbill'+str(
+                                    baseyear-4): 'ContentPlaceHolder1_TaxBillInfo_rptTaxBill_taxBillAmount_4'},
+                                {'homeownerexemption': 'ContentPlaceHolder1_TaxCalculator_lblHomeownerExemption'},
+                                {'seniorfreezeexemption': 'ContentPlaceHolder1_TaxCalculator_lblSeniorFreezeExemption'},
+                                {'seniorcitizenexemption': 'ContentPlaceHolder1_TaxCalculator_lblSeniorCitizenExemption'},
+                                {'taxbeforeexemptions': 'ContentPlaceHolder1_TaxCalculator_lblTaxBeforeExemptions'},
+                                {'taxafterexemptions': 'ContentPlaceHolder1_TaxCalculator_lblTaxAfterExemptions'},
+                                {'estimatedvalue': 'ContentPlaceHolder1_TaxYearInfo_propertyEstimatedValue'},
+                                {'assessedvalue': 'ContentPlaceHolder1_TaxYearInfo_propertyAssessedValue'},
+                                {'lotsize': 'ContentPlaceHolder1_TaxYearInfo_propertyLotSize'},
+                                {'buildingsize': 'ContentPlaceHolder1_TaxYearInfo_propertyBuildingSize'},
+                                {'class': 'ContentPlaceHolder1_TaxYearInfo_propertyClass'},
+                                {'taxcode': 'ContentPlaceHolder1_TaxYearInfo_propertyTaxCode'},
+                                {'address': 'ContentPlaceHolder1_PropertyInfo_propertyAddress'},
+                                {'city': 'ContentPlaceHolder1_PropertyInfo_propertyCity'},
+                                {'zip': 'ContentPlaceHolder1_PropertyInfo_propertyZip'},
+                                {'township': 'ContentPlaceHolder1_PropertyInfo_propertyTownship'},
+                                {'pin': 'ContentPlaceHolder1_lblResultTitle'}
+                                ]
 
-                    except splinter.exceptions.ElementDoesNotExist:
-                        print('Error processing '+str(pin.PIN) + \
-                            ' ' + list(item.keys())[0] + ' ' + url + '')
+                    row = pd.DataFrame()
+                    for item in items:
+                        try:
+                            value = browser.find_by_id(
+                                list(item.values())[0]).first.html
+                            row[list(item.keys())[0]] = [value]
 
-                results = results.append(row)
-        # print results
-        results.to_csv(str(taxyear)+'/cookcountyassessments.csv')
+                        except splinter.exceptions.ElementDoesNotExist:
+                            row[list(item.keys())[0]] = ""
+                            print('Error processing '+str(pin.PIN) + \
+                                ' ' + list(item.keys())[0] + ' ' + url + '')
+
+                    results = results.append(row)
+                # print results
+                print("save")
+                results.to_csv(str(taxyear)+'/cookcountyassessments.csv', index=False)
+        except selenium.common.exceptions.WebDriverException:
+            retries = retries + 1
+            if (retries==10):
+                raise
+            valid=False
+            print('Error, retry ' + str(retries))
+            time.sleep(5)
